c723cd5d5bdd41b20f3e0388e2178af4d40bab2f
Srinath Gosakan <gosakan003@gmail.com>
2025-06-24 22:34:29 +0530

    Backend deployment testing

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URI)
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: process.env.CLIENT_URL, 
   credentials: true,               
 }));
 app.use(session({
